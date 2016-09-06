3013bb84fe046fd91519c5896ff9206b4c1bf715
sdluxeon <sd.luxeon@gmail.com>
2016-09-06 11:20:47 +0300

    Ignored Packages

---
+++ src/Elders.Skynet.Host/PackageExplorer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Elders.Skynet.Core.Handlers.Packages;
+using Elders.Skynet.Core.Packages;
+
+namespace Elders.Skynet.Host
+{
+    public class PackageExplorer : IPackageExplorer
+    {
+        private List<IExecutableLocator> locators;
+
+        public PackageExplorer(params IExecutableLocator[] locators)
+        {
+            this.locators = locators.ToList();
+        }
+
+        public IExectuable GetExecutable(PackageMeta package)
+        {
+            var location = string.Empty;
+            if (!string.IsNullOrEmpty(package.Exectuable))
+                location = package.Exectuable;
+            else
+                location = package.Location;
+            foreach (var item in locators)
+            {
+                var result = item.Locate(location);
+                if (result != null)
+                    return result;
+            }
+            throw new InvalidOperationException("Could not locate executable");
+        }
+
+    }
+}
---
+++ src/Elders.Skynet.Host/SimpleExecutableLocator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Elders.Skynet.Core.Handlers.Packages;
+
+namespace Elders.Skynet.Host
+{
+    public interface IExecutableLocator
+    {
+        IExectuable Locate(string location);
+    }
+
+    public class SimpleExecutableLocator : IExecutableLocator
+    {
+        private string serverLocation;
+
+        private int port;
+
+        public SimpleExecutableLocator(string location, int port)
+        {
+            this.serverLocation = location;
+            this.port = port;
+        }
+
+        public IExectuable Locate(string location)
+        {
+            if (File.Exists(location))
+            {
+                return new SimpleExecutable(location, new string[] { serverLocation, port.ToString() });
+            }
+            else
+            {
+                var files = Directory.GetFiles(location).Where(x => x.EndsWith(".exe"));
+                if (files.Count() > 1)
+                    throw new InvalidOperationException("More than one executable found");
+                if (files.Count() == 1)
+                    return new SimpleExecutable(files.First(), new string[] { serverLocation, port.ToString() });
+                else
+                    return null;
+            }
+        }
+
+        public class SimpleExecutable : IExectuable
+        {
+            public SimpleExecutable(string file, string[] args)
+            {
+                string codeBase = typeof(Elders.Skynet.Subprocess.SimpleClient).Assembly.CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                ExecutableLocation = Uri.UnescapeDataString(uri.Path);
+                var arguments = new string[] { "simple-client", file }.ToList();
+                arguments.AddRange(args);
+                this.Args = arguments.ToArray();
+
+            }
+
+            public string ExecutableLocation { get; set; }
+
+            public string[] Args { get; set; }
+        }
+    }
+}
